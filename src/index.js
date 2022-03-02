ce8d6ea912e37f66918caa28bf8670615c7ae157
nachob14 <98466036+nachob14@users.noreply.github.com>
2022-03-02 19:34:45 -0300

    Se agregan funciones para cargar y mostrar las distintas divisas

--- src/index.js
+++ src/index.js
@@ -6,9 +6,31 @@ const API_KEY = '230d955d9e3d5a7df9037eb8';
 // estructura = `${BASE_URL}${API_KEY}/${FECHA}/{$BASE}`
 // codigos soportados https://v6.exchangerate-api.com/v6/YOUR-API-KEY/codes
 
-
-function obtenerMonedas(){
-    //fetch(`${BASE_URL}${API_KEY}/codes`);
-
-    return console.log(`${BASE_URL}${API_KEY}/codes`)
+function inicializar(){
+    mostrarMonedas();
 }
+
+
+async function obtenerMonedas(){
+    try{
+        const respuesta = await fetch(`${BASE_URL + API_KEY}/codes`);
+        const datos = await respuesta.json();
+        return datos.supported_codes;
+    } catch (error) {
+        console.error('Error', error);
+    }    
+}
+
+function mostrarMonedas(){
+    obtenerMonedas().then(monedas => {
+        monedas.forEach(moneda => {
+            const form = document.querySelector('#inputSelectorMoneda')
+            const eleccion = document.createElement('option');
+            eleccion.setAttribute('value', moneda[0]);
+            eleccion.textContent = `${moneda[0]} ${moneda[1]}`;
+            form.appendChild(eleccion);
+        });
+    });
+}
+
+inicializar();
